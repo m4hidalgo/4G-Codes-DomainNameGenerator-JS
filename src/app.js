67e827f838b01921e471a71135d962fe005ac514
miguelangelhidalgo <miguelangel.hidalgoarroyo@gmail.com>
2022-11-26 19:39:48 +0000

    update commit

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
+//import "./style.css";
 
 //import "./assets/img/rigo-baby.jpg";
 //import "./assets/img/4geeks.ico";
@@ -9,22 +8,15 @@ let pronoun = ["the", "our"];
 let adj = ["great", "big"];
 let noun = ["jogger", "racoon"];
 let webDomain = [".com", ".net", ".es", ".io"];
-let newDomain = "";
+let newDomain = [];
 
-let tgtDomain = document.querySelector("#domain");
-
-window.onload = function() {
-  //write your code here
-  setDomain();
-  tgtDomain.innerHTML = newDomain;
-};
-
+//write your code here
 const setDomain = () => {
   for (let i = 0; i < pronoun.length; i++) {
     for (let j = 0; j < adj.length; j++) {
       for (let k = 0; k < noun.length; k++) {
         for (let l = 0; l < webDomain.length; l++) {
-          newDomain += pronoun[i] + adj[j] + noun[k] + webDomain[l] + "<br>";
+          newDomain.push(pronoun[i] + adj[j] + noun[k] + webDomain[l]);
         }
       }
     }
@@ -32,3 +24,6 @@ const setDomain = () => {
 
   return newDomain;
 };
+
+setDomain();
+console.log(newDomain);
