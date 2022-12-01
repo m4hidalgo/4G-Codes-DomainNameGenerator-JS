55d9c427e41f163e0cf6424db6308e73b7fc1365
miguelangelhidalgo <miguelangel.hidalgoarroyo@gmail.com>
2022-12-01 19:36:06 +0000

    second update commit

--- src/app.js
+++ src/app.js
@@ -8,7 +8,6 @@ let pronoun = ["the", "our"];
 let adj = ["great", "big"];
 let noun = ["jogger", "racoon"];
 let webDomain = [".com", ".net", ".es", ".io"];
-let newDomain = [];
 
 //write your code here
 const setDomain = () => {
@@ -16,14 +15,11 @@ const setDomain = () => {
     for (let j = 0; j < adj.length; j++) {
       for (let k = 0; k < noun.length; k++) {
         for (let l = 0; l < webDomain.length; l++) {
-          newDomain.push(pronoun[i] + adj[j] + noun[k] + webDomain[l]);
+          console.log(pronoun[i] + adj[j] + noun[k] + webDomain[l]);
         }
       }
     }
   }
-
-  return newDomain;
 };
 
 setDomain();
-console.log(newDomain);
